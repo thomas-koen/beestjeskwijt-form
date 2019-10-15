4b736af3aecc06d18c477b0a49bc7ad2b8162202
Rhytz <ik@koenreus.com>
2019-10-15 12:14:36 +0200

    initial commit

---
+++ beestjeskwijt-form.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Beestjeskwijt Formulier
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Formulier voor Beestjeskwijt affiliate
+ * Author:          tyCoon Media
+ * Author URI:      https://tycoonmedia.net
+ * Text Domain:     beestjeskwijt-form
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Beestjeskwijt_Form
+ */
+
+// Your code starts here.
